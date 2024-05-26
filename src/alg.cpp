a86e29101e5cf8dc035b54e00bc21bdc2263306d
Kryazheva Valeria <150891739+vallerakr@users.noreply.github.com>
2024-05-26 18:29:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
+// Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
 #include <sstream>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
     BST<std::string> tree;
     std::ifstream file(filename);
 
