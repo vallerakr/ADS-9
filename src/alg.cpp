cc11e9474905fa2b994f1a9624f85a1ed3f0f8ab
Kryazheva Valeria <150891739+vallerakr@users.noreply.github.com>
2024-05-26 16:35:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,36 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+bool isLetter(char* ch) {
+    int letterIndex = 0;
+    for (int i = 65; i < 91; ++i) {
+        if (*ch == i) {
+            *ch = 97 + letterIndex;
+            return true;
+        }
+        ++letterIndex;
+    }
+    for (int i = 97; i < 123; ++i) {
+        if (*ch == i) {
+            return true;
+        }
+    }
+    return false;
+}
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    std::string currentWord;
+    BST<std::string> bst;
+    while (!file.eof()) {
+        char ch = file.get();
+        if (!isLetter(&ch)) {
+            bst.add(currentWord);
+            currentWord = "";
+        } else if (isLetter(&ch)) {
+            currentWord += ch;
+        }
+    }
+    file.close();
+    return bst;
 }
